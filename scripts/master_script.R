4407b56147eab8f21ec831a0c5ccde64dc53e71a
Garland Xie <garlandxie@gmail.com>
2021-12-15 21:47:26 -0500

    Added script for processing fall data

--- scripts/master_script.R
+++ scripts/master_script.R
@@ -4,3 +4,4 @@ library(here)
 # Data processing ----
 
 source(here("scripts", "processing_scripts", "01-import_spring_data.R"))
+source(here("scripts", "processing_scripts", "02-import_fall_data.R"))
