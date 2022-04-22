0b0afb02c357fdc003c0d6d50c09530c714e340e
Garland Xie <garlandxie@gmail.com>
2022-04-22 11:17:07 -0400

    Major changes

--- scripts/analysis_scripts/01-glm.R
+++ scripts/analysis_scripts/01-glm.R
@@ -5,6 +5,9 @@ library(ggplot2)
 library(performance)
 library(MASS)
 library(patchwork)
+library(lme4)
+library(emmeans)
+library(tidyr)
 
 # import ----
 
@@ -18,6 +21,12 @@ fall_sb <- read.csv(
   row.names = 1
 )
 
+
+sb_taxon <- read.csv(
+  here("data", "input_data", 
+       "meadoway_seed_bank_taxonomy.csv")
+)
+
 # check packaging ----
 glimpse(spr_sb)
 glimpse(fall_sb)
@@ -25,6 +34,126 @@ glimpse(fall_sb)
 # clean data ----
 
 sb <- rbind(fall_sb, spr_sb)
+
+## table ----
+
+# summarize: species as rows, site as columns, cells as abundances
+table_summary <- sb %>%
+  group_by(site, spp_code) %>%
+  summarize(abundance = sum(total_abund, na.rm = TRUE)) %>%
+  ungroup() %>%
+  tidyr::pivot_wider(names_from = site, values_from = abundance) %>%
+  mutate(across(everything(), ~replace_na(.x, 0))) %>%
+  left_join(sb_taxon, by = c("spp_code" = "Code")) %>%
+  dplyr::select(
+    spp_code, Genus, Species, 
+    VICP, TIMH, KENN, 
+    GRNB, BNSH, DAVE, 
+    DVAG, AMBJ, BRIM
+    )
+
+write.csv(
+  x = table_summary, 
+  here("data", "analysis_data", "table_devlin.csv"), 
+)
+
+## total germination ----
+
+# spring and fall germination total emergence for each treatment
+total_germ_by_trt <- sb %>%
+  group_by(season, treatment) %>%
+  summarize(abund = sum(total_abund, na.rm = TRUE)) %>%
+  ungroup()
+
+write.csv(
+  x = total_germ_by_trt, 
+  file = here("data", "intermediate_data", "total_germ_by_sn_trt.csv")
+)
+
+## obtain species identity for plot outliers -----
+
+### BRIM Spring 11 ----
+out_brim_spr <- sb %>%
+  filter(site == "BRIM" & season == "Spring") %>%
+  group_by(season, site, treatment, plot) %>%
+  summarize(abund = sum(total_abund, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  arrange(desc(abund))
+
+out_brim_spr_11 <- sb %>%
+  filter(site == "BRIM" & season == "Spring" & plot == 11) 
+
+write.csv(
+  x = out_brim_spr_11, 
+  file = here("data", "intermediate_data", "out_brim_spr_11.csv")
+)
+
+### BNSH Spring 15 ----
+out_bnsh_spr <- sb %>%
+  filter(site == "BNSH" & season == "Spring") %>%
+  group_by(season, site, treatment, plot) %>%
+  summarize(abund = sum(total_abund, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  arrange(desc(abund))
+
+out_bnsh_15 <- sb %>%
+  filter(site == "BNSH" & season == "Spring" & plot == 15) 
+
+write.csv(
+  x = out_bnsh_15, 
+  file = here("data", "intermediate_data", "out_bnsh_spr_15.csv")
+)
+
+### BRIM Fall 11 ----
+
+out_brim_fall <- sb %>%
+  filter(site == "BRIM" & season == "Fall") %>%
+  group_by(season, site, treatment, plot) %>%
+  summarize(abund = sum(total_abund, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  arrange(desc(abund))
+
+out_brim_fall_11 <- sb %>%
+  filter(site == "BRIM" & season == "Fall" & plot == 11) 
+
+write.csv(
+  x = out_brim_fall_11, 
+  file = here("data", "intermediate_data", "out_brim_fall_11.csv")
+)
+
+### AMBJ Fall ----
+
+out_ambj_fall <- sb %>%
+  filter(site == "AMBJ" & season == "Fall") %>%
+  group_by(season, site, treatment, plot) %>%
+  summarize(abund = sum(total_abund, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  arrange(desc(abund))
+
+out_ambj_fall_23 <- sb %>%
+  filter(site == "AMBJ" & season == "Fall" & plot == 23) 
+
+write.csv(
+  x = out_ambj_fall_23, 
+  file = here("data", "intermediate_data", "out_ambj_fall_23.csv")
+)
+
+### TIMH Fall ----
+
+out_timh_fall <- sb %>%
+  filter(site == "TIMH" & season == "Fall") %>%
+  group_by(season, site, treatment, plot) %>%
+  summarize(abund = sum(total_abund, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  arrange(desc(abund))
+
+out_timh_fall_5 <- sb %>%
+  filter(site == "TIMH" & season == "Fall" & plot == 5) 
+
+write.csv(
+  x = out_timh_fall_5, 
+  file = here("data", "intermediate_data", "out_timh_fall_5.csv")
+)
 
 # get community-level abundance and species richness
 sb_comm <- sb %>%
@@ -43,7 +172,7 @@ sb_comm <- sb %>%
         "GRNB", "DAVE", "BNSH", 
         "DVAG", "BRIM", "AMBJ")
       ), 
-    treatment = factor(treatment)
+    treatment = factor(treatment, levels = c("RES", "MOW", "TIL"))
   )
 
 # exploring data (before regression) -----
@@ -130,62 +259,130 @@ sb_comm %>%
 
 ## abundance ----
 
-glm_abund_poisson <- glm(
-  formula = abund ~ treatment + season + site, 
+glmer_abund_poisson <- glmer(
+  formula = abund ~ season + treatment + (1|site), 
   family = poisson(link = "log"), 
   data = sb_comm
   )
 
-# check assumption of poisson regression models 
-check_overdispersion(glm_abund_poisson)
+### check overdispersion ----
+check_overdispersion(glmer_abund_poisson)
 
-glm_abund_nb <- glm.nb(
-  formula = abund ~ treatment + season + site, 
+### account for overdispersion ----
+glmer_abund_nb <- glmer.nb(
+  formula = abund ~ treatment + season + (1|site), 
   data = sb_comm
 )
 
-summary(glm_abund_nb)
+### model summary ----
+summary(glmer_abund_nb)
+
+### check diagnostics ----
+check_model(glmer_abund_nb)
+
+### r-squared ----
+r2(glmer_abund_nb)
+
+### pairwise comparison ----
+abund_emm_trt <- emmeans(
+  glmer_abund_nb, 
+  "treatment", 
+  lmer.df = "satterthwaite"
+  )
+
+abund_emm_sn <- emmeans(
+  glmer_abund_nb, 
+  "season", 
+  lmer.df = "satterhwaite"
+)
+
+# need to get mow vs til comparisons here
+abund_pairs_trt <- abund_emm_trt %>%
+  pairs() %>%
+  as.data.frame()
+
+abund_pairs_sn <- abund_emm_sn %>%
+  pairs() %>%
+  as.data.frame()
 
 ## species richness ----
 
-glm_sr_poisson <- glm(
-  formula = species_richness ~ treatment + season + site, 
-  family = poisson(link = "log"), 
+glmer_richness <- glmer(
+  formula = species_richness ~ treatment + season + (1|site), 
+  family = poisson(link = "log"),
   data = sb_comm
 )
 
-# check assumption of poisson regression models 
-check_overdispersion(glm_sr_poisson)
+### check overdispersion ----
+check_overdispersion(glmer_richness)
 
-summary(glm_sr_poisson)
+### check model diagnostics ----
+
+# homogeneity of variance
+# collinearity
+# influential observations
+# normal of residuals 
+# normality of random effects
+check_model(glmer_richness)
+
+### model summary -----
+summary(glmer_richness)
+
+### r squared ----
+r2(glmer_richness)
+
+### pairwise comparisons ----
+richness_emm_trt <- emmeans(
+  glmer_richness, 
+  "treatment", 
+  lmer.df = "satterthwaite"
+)
+
+richness_emm_sn <- emmeans(
+  glmer_richness, 
+  "season", 
+  lmer.df = "satterhwaite"
+)
+
+# need to get mow vs til comparisons here
+richness_pairs_trt <- richness_emm_trt %>%
+  pairs() %>%
+  as.data.frame()
+
+richness_pairs_sn <- richness_emm_trt %>%
+  pairs() %>%
+  as.data.frame()
 
 ## save to disk ----
 ggsave(
-  filename = here("output", "results", "seedling_abundance.png"), 
-  plot = ggplot_abund, 
-  device = "png", 
-  units = "in", 
-  height = 5, 
-  width = 7
-)
-
-ggsave(
-  filename = here("output", "results", "seedling_richness.png"), 
-  plot = ggplot_sr, 
-  device = "png", 
-  units = "in", 
-  height = 5, 
-  width = 7
-)
-
-ggsave(
-  filename = here("output", "results", "multi_plot.png"), 
+  filename = here("output", "results", "multi_plot.svg"), 
   plot = multi_plot, 
-  device = "png", 
+  device = "svg", 
   units = "in", 
   height = 4, 
   width = 9
 )
+
+write.csv(
+  abund_pairs_trt, 
+  file = here("output", "results", "abund_pairs_trt.csv")
+)
+
+write.csv(
+  abund_pairs_sn, 
+  file = here("output", "results", "abund_pairs_sn.csv")
+)
+
+write.csv(
+  richness_pairs_trt, 
+  file = here("output", "results", "richness_pairs_trt.csv")
+)
+
+write.csv(
+  richness_pairs_sn, 
+  file = here("output", "results", "richness_pairs_sn.csv")
+)
+
 
 
 
