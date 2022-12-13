ad6c815e3457f989bec37a4a098b8acae55ba277
Garland Xie <garlandxie@gmail.com>
2022-12-13 17:35:54 -0500

    Initial commit

---
+++ scripts/data_appendix_scripts/03_summarize_table.R
@@ -0,0 +1,51 @@
+# libraries --------------------------------------------------------------------
+library(here)   # for creating relative file-paths
+library(dplyr)  # for manipulating data 
+library(tidyr)  # for making long to wide tables
+
+# import -----------------------------------------------------------------------
+
+## seed banks ------------------------------------------------------------------
+sb_spr <- read.csv(
+  here("data", "analysis_data", "spring_seedbank.csv"),
+  row.names = 1
+)
+
+sb_fall <- read.csv(
+  here("data", "analysis_data", "fall_seedbank.csv"),
+  row.names = 1
+)
+
+## taxonomy --------------------------------------------------------------------
+sb_taxon <- read.csv(
+  here("data", "input_data", 
+       "seed_bank_taxonomy.csv")
+)
+
+# clean data -------------------------------------------------------------------
+
+sb <- rbind(sb_spr, sb_fall)
+
+abund_by_site <- sb %>%
+  full_join(sb_taxon, by = c("spp_code" = "Code")) %>%
+  filter(!(is.na(plot))) %>%
+  select(season, site_code, plot, spp_code, Genus, Species, total_abund) %>%
+  group_by(season, site_code, spp_code, Genus, Species) %>%
+  summarize(abund = sum(total_abund)) %>%
+  ungroup()
+
+abund_by_site_long <- abund_by_site %>%
+  group_by(season, site_code, spp_code, Genus, Species) %>%
+  tidyr::pivot_wider(names_from = site_code, values_from = abund) %>%
+  ungroup() %>%
+  arrange(desc(season), spp_code) %>%
+  mutate(across(M4_1:T2_3, ~replace_na(.x ,0)))
+
+# save to disk -----------------------------------------------------------------
+
+write.csv(
+  x = abund_by_site_long, 
+  file = here("output", "data_appendix_output", "tables2_summary_table.csv")
+)
+
+
